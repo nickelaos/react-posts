9052f99590e0e73f7bee385984e05026335c5d4e
nick.gutsuliak <nick.gutsuliak@sunbeez.com>
2019-08-19 10:07:46 +0300

    Updates

---
+++ precache-manifest.86c9f30f7f59e3fb3c1a87f3cc4f5a29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45009ada3aa89a40f204aedeb683cad6",
+    "url": "/react-posts/index.html"
+  },
+  {
+    "revision": "8f20c9665c9a60885ec6",
+    "url": "/react-posts/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "ae1883234fa61fdccf9d",
+    "url": "/react-posts/static/css/main.639ecc08.chunk.css"
+  },
+  {
+    "revision": "8f20c9665c9a60885ec6",
+    "url": "/react-posts/static/js/2.02ec0e73.chunk.js"
+  },
+  {
+    "revision": "ae1883234fa61fdccf9d",
+    "url": "/react-posts/static/js/main.210198a9.chunk.js"
+  },
+  {
+    "revision": "f39ab604de9bbe243b54",
+    "url": "/react-posts/static/js/runtime~main.3249532b.js"
+  }
+]);
